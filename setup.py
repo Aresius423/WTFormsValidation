8ec41ea153995245e99694ff0ef587bf6114aeb4
Márkus János Dániel <aresius423@gmail.com>
2020-05-04 12:30:26 +0200

    Update setup for v0.1.0

--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="WTFormsValidation-aresius", # Replace with your own username
-    version="0.1",
+    name="WTFormsValidation",
+    version="0.1.0",
     author="János Márkus",
     author_email="aresius423@gmail.com",
-    description="A python library for generating client side validation functions from WTForms for FormValidation",
+    description="A python library for generating client side validation functions from WTForms for various validator libraries.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Aresius423/WTFormsValidation",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'wtforms'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
